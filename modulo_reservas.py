4d08cf897b8cf0aaa29ebe8ad329b11f95d3fb58
Ivan <ivan_alejandro_a@hotmail.com>
2026-05-25 18:27:18 -0300

    add
    prueba

--- modulo_reservas.py
+++ modulo_reservas.py
@@ -1,3 +1,5 @@
+def 
+
 def main():
 
         opcion = -1
@@ -5,10 +7,8 @@ def main():
 
         while opcion != 0:
             print("\n")
-            print("=== MENU PRINCIPAL ===")
+            print("=== RESERVAS ===")
             print("1. Operaciones con listas")
-            print("2. Analisis de listas")
-            print("3. Trabajar con frases")
             print("0. Salir")
             print("Elegi una opcion:")
             opcion = int(input())
@@ -17,13 +17,11 @@ def main():
                 case 1:
                     submenu_listas(lista)
                 case 2:
-                    print("Analisis de listas")
-                case 3:
-                    print("Trabajar con frases")
+                      
                 case 0:
                     print("\n*Usted salio del programa*")
                     break
                 case _:  # como el default: en c
                     print("opcion invalida, vuelve a intentarlo")
-                    
+
 main()
